8331f651db1b3bbf09cd5db1868cc28a0dd02079
Nicolai <nicolai.verbaarschot@gmail.com>
2023-06-18 21:11:16 +0200

    Specified vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import seqPreprocessor from "svelte-sequential-preprocessor";
 import { preprocessThrelte } from "@threlte/preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()]),
 
   kit: {
