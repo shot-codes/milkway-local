459e14942644ac97c43121d2fc4bdb5ee9213664
Nicolai <nicolai.verbaarschot@gmail.com>
2023-06-26 18:02:21 +0200

    Generalized error handling in completion chat.

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -2,61 +2,60 @@ import type { RequestHandler } from "./$types";
 import { getClientInstance as getOpenAIClient } from "$lib/server/openai";
 import { getClientInstance as getPineconeClient } from "$lib/server/pinecone";
 import type { ScoredVector } from "@pinecone-database/pinecone";
-import { json } from "@sveltejs/kit";
+import { json, error } from "@sveltejs/kit";
 
 const openaiClient = getOpenAIClient();
 const pineconeClient = await getPineconeClient();
 const index = pineconeClient.Index("milkyway");
 
 export const POST = (async ({ request }) => {
-  const data = await request.json();
-  const query = data.msg;
+  try {
+    const data = await request.json();
+    const query = data.msg;
 
-  const xq = await openaiClient.createEmbedding({
-    model: "text-embedding-ada-002",
-    input: String(query),
-  });
+    const xq = await openaiClient.createEmbedding({
+      model: "text-embedding-ada-002",
+      input: String(query),
+    });
 
-  const res = await index.query({
-    queryRequest: {
-      topK: 1,
-      vector: xq["data"]["data"][0]["embedding"],
-      includeValues: false,
-      includeMetadata: true,
-    },
-  });
+    const res = await index.query({
+      queryRequest: {
+        topK: 1,
+        vector: xq["data"]["data"][0]["embedding"],
+        includeValues: false,
+        includeMetadata: true,
+      },
+    });
 
-  const contexts = res.matches?.map((x: ScoredVector) => {
-    if (x.metadata && "text" in x.metadata) {
-      return x.metadata.text;
-    }
-  });
+    const contexts = res.matches?.map((x: ScoredVector) => {
+      if (x.metadata && "text" in x.metadata) {
+        return x.metadata.text;
+      }
+    });
 
-  const limit = 3750;
-  let prompt = "";
+    const limit = 3750;
+    let prompt = "";
 
-  // If confidence is high, add context.
-  console.log(res.matches?.[0].score);
-  if (res.matches?.[0].score && res.matches[0].score > 0.75) {
-    const prompt_start = "Answer the question based on the context below.\n\n" + "Context:\n";
-    const prompt_end = `\n\nQuestion: ${query}\nAnswer:`;
-    if (contexts) {
-      for (let i = 0; i < contexts.length; i++) {
-        if (contexts.slice(0, i).join("\n\n---\n\n").length >= limit) {
-          prompt = prompt_start + contexts.slice(0, i - 1).join("\n\n---\n\n") + prompt_end;
-          break;
-        } else if (i == contexts.length - 1) {
-          prompt = prompt_start + contexts.join("\n\n---\n\n") + prompt_end;
+    // If confidence is high, add context.
+    if (res.matches?.[0].score && res.matches[0].score > 0.75) {
+      const prompt_start = "Answer the question based on the context below.\n\n" + "Context:\n";
+      const prompt_end = `\n\nQuestion: ${query}\nAnswer:`;
+      if (contexts) {
+        for (let i = 0; i < contexts.length; i++) {
+          if (contexts.slice(0, i).join("\n\n---\n\n").length >= limit) {
+            prompt = prompt_start + contexts.slice(0, i - 1).join("\n\n---\n\n") + prompt_end;
+            break;
+          } else if (i == contexts.length - 1) {
+            prompt = prompt_start + contexts.join("\n\n---\n\n") + prompt_end;
+          }
         }
       }
+    } else {
+      prompt =
+        "You are a chatbot assistant for a website. You're goal is to answer questions about the website experience. Answer the following accordingly:\n" +
+        String(query);
     }
-  } else {
-    prompt =
-      "You are a chatbot assistant for a website. You're goal is to answer questions about the website experience. Answer the following accordingly:\n" +
-      String(query);
-  }
 
-  try {
     const completion = await openaiClient.createCompletion({
       model: "text-davinci-003",
       prompt,
@@ -65,24 +64,10 @@ export const POST = (async ({ request }) => {
     return json(completion.data.choices[0].text);
 
     // TODO: send potential errors to client.
-  } catch (error: unknown) {
-    if (typeof error == "object" && error !== null && "response" in error) {
-      if (
-        typeof error.response == "object" &&
-        error.response !== null &&
-        "status" in error.response &&
-        "data" in error.response
-      ) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      }
-    } else if (typeof error == "object" && error !== null && "message" in error) {
-      console.log(error.message);
-    } else {
-      console.log("Unknown error:\n");
-      console.log(error);
-    }
+  } catch (err) {
+    console.error(err);
+    throw error(503, {
+      message: "Error proccessing request.",
+    });
   }
-
-  return json("I'm sorry, I wasn't able to generate an answer for you");
 }) satisfies RequestHandler;
