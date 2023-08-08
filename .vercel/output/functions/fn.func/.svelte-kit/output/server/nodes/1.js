bc41ead282456c2cd525690beae6083a56ca1734
Nicolai <nicolai.verbaarschot@gmail.com>
2023-08-08 10:07:34 +0200

    removed .vercel.

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.37766d95.js","_app/immutable/chunks/scheduler.bea104e2.js","_app/immutable/chunks/index.520d9713.js","_app/immutable/chunks/singletons.a723794f.js","_app/immutable/chunks/index.b6975e4a.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/nodes/4.fe3065fc.js
+++
@@ -1,1 +0,0 @@
-import{bm as e}from"../chunks/4.133a45dd.js";export{e as component};
